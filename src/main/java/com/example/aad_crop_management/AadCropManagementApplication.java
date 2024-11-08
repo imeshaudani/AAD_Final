7e172b90352e2f9a947c6d3d6fab3e12118dc86a
imesha <imeshaudani2001@gmail.com>
2024-11-08 12:17:58 +0530

    initial commit

---
+++ src/main/java/com/example/aad_crop_management/AadCropManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.aad_crop_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadCropManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadCropManagementApplication.class, args);
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/com/example/aad_crop_management/AadCropManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aad_crop_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadCropManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
