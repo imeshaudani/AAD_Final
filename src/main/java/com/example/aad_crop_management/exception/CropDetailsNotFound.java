ba0c62f9486e7377f4970dd1e61b9a9a9187f74d
imesha <imeshaudani2001@gmail.com>
2024-11-16 23:50:23 +0530

    UserNotFound exception and CropDetailsNotFound added

---
+++ src/main/java/com/example/aad_crop_management/exception/CropDetailsNotFound.java
@@ -0,0 +1,7 @@
+package com.example.aad_crop_management.exception;
+
+public class CropDetailsNotFound extends RuntimeException{
+    public CropDetailsNotFound(){}
+    public CropDetailsNotFound(String message){}
+    public CropDetailsNotFound(String message, Throwable cause){}
+}
---
+++ src/main/java/com/example/aad_crop_management/exception/UserNotFound.java
@@ -0,0 +1,7 @@
+package com.example.aad_crop_management.exception;
+
+public class UserNotFound extends RuntimeException{
+    public UserNotFound(){}
+    public UserNotFound(String message){}
+    public UserNotFound(String message, Throwable cause){}
+}
