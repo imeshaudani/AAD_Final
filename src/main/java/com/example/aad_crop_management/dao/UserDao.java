7a75644c07f47c91d07adea7dc5efb9f9329840c
imesha <imeshaudani2001@gmail.com>
2024-11-19 14:29:23 +0530

    Process setUserDetailsService in AuthenticationProvider

--- src/main/java/com/example/aad_crop_management/dao/UserDao.java
+++ src/main/java/com/example/aad_crop_management/dao/UserDao.java
@@ -4,7 +4,9 @@ import com.example.aad_crop_management.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
 
+@Repository
 public interface UserDao extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
 }
